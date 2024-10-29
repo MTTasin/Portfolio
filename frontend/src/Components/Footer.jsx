d3bbede8bfb80922651044bad13333651b346691
M.T. Tasin <m.t.tasin20@gmail.com>
2024-10-29 21:02:22 +0600

    Done with the footer

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,16 +1,87 @@
-import { Link } from "react-router-dom";
-import { Clipboard } from "flowbite-react";
+import { IoCopyOutline, IoCallOutline } from "react-icons/io5";
+import { LuCopyCheck, LuGithub, LuTwitter, LuLinkedin } from "react-icons/lu";
+import { CiMail } from "react-icons/ci";
+import { useState } from "react";
+import copy from "copy-to-clipboard";
+import { Tooltip } from "flowbite-react";
 
 export default function Footer() {
+  const [copymail, setCopymail] = useState(false);
+  const [copycall, setCopycall] = useState(false);
+
+  function copyEmail() {
+    copy("m.t.tasin20@gmail.com");
+    setCopymail(true);
+    // after 2 sec set copymail to false
+    setTimeout(() => {
+      setCopymail(false);
+    }, 2000);
+  }
+  function copyCall() {
+    copy("+8801974283081");
+    setCopycall(true);
+    // after 2 sec set copymail to false
+    setTimeout(() => {
+      setCopycall(false);
+    }, 2000);
+  }
+
   return (
-    <footer className="p-10 bg-white text-white">
-      <div>
-        <Clipboard
-          valueToCopy="npm install flowbite-react"
-          value="hello"
-          className="bg-red-500"
-        />
+    <footer className="p-10 bg-black text-white">
+      <div className="flex flex-col justify-center items-center">
+        <div className="bg-gray-500 text-black text-xl rounded-full px-5 py-2">
+          Get in touch
+        </div>
+        <div className="text-center my-5">
+          <p>
+            What's next? Feel free to reach out to me if you're looking for a
+            developer, <br /> have a query, or simply want to connect.{" "}
+          </p>
+        </div>
+        <div className="flex justify-center items-center gap-2 my-5">
+          <span>
+            <CiMail className="text-3xl" />
+          </span>
+          <span className="text-3xl font-bold">m.t.tasin20@gmail.com</span>
+          <span>
+            {copymail ? (
+              <Tooltip content="Copied"><LuCopyCheck className="text-3xl" /></Tooltip>
+            ) : (
+              <Tooltip content="Copy Email"><IoCopyOutline className="text-3xl" onClick={copyEmail} /></Tooltip>
+            )}
+          </span>
+        </div>
+        <div className="flex justify-center items-center gap-2 my-5">
+          <span>
+            <IoCallOutline className="text-3xl" />
+          </span>
+          <span className="text-3xl font-bold">+880 1974 28 30 81</span>
+          <span>
+            {copycall ? (
+              <Tooltip content="Copied"><LuCopyCheck className="text-3xl" /></Tooltip>
+            ) : (
+              <Tooltip content="Copy Phone Number"><IoCopyOutline className="text-3xl" onClick={copyCall} /></Tooltip>
+            )}
+          </span>
+        </div>
+
+        <div>
+          <p>You may also find me on this platforms!</p>
+        </div>
+        <div className="flex justify-center items-center gap-10 my-5">
+          <span>
+            <a href="https://github.com/MTTasin" target="_blank"><LuGithub className="text-3xl" /></a>
+          </span>
+          <span>
+            <a href="https://x.com/MTTasin1" target="_blank"><LuTwitter className="text-3xl" /></a>
+          </span>
+          <span>
+            <a href="https://www.linkedin.com/in/mttasin/" target="_blank"><LuLinkedin className="text-3xl" /></a>
+          </span>
+        </div>
       </div>
     </footer>
   );
 }
+
+// {copymail ?  (<LuCopyCheck className="text-3xl" />) : (<IoCopyOutline className="text-3xl" onClick={copyEmail} />)}
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,51 +1,96 @@
-import { useState } from "react"
-import { NavLink, Link } from "react-router-dom"
+import { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 import { Button, Drawer, Navbar } from "flowbite-react";
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter, FaX } from "react-icons/fa6";
+import { FaXTwitter, FaX, FaLinkedin } from "react-icons/fa6";
 import { FiGithub } from "react-icons/fi";
 import { TbMenuDeep } from "react-icons/tb";
 
-
-
-
-
-export default function Header (){
-
+export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClose = () => setIsOpen(false);
 
+  const nav = document.getElementById("navbar");
+  // nav.classList.remove("absolute", window.scrollY > 100);
+
+  window.onscroll = () => {
+    if (window.scrollY > 100) {
+      nav.classList.add("fixed");
+      nav.classList.remove("absolute");
+    } else {
+      nav.classList.add("absolute");
+      nav.classList.remove("fixed");
+    }
+  };
+
   return (
     <>
-      <Navbar className="navbar absolute top-0 flex items-center justify-beetween bg-black w-full mx-auto">
-        <div  className="navbar-start md:mx-20 my-10"><Link to="/"><img src="/logo.png" alt="M.T. Tasin" className="w-[40px] h-[40px] md:w-[40px] md:h-[40px]" /></Link></div>
-        <div className="navbar-end md:mx-20 my-10">
-        <TbMenuDeep className="text-white text-3xl" onClick={() => setIsOpen(!isOpen)} />
+      <Navbar
+        id="navbar"
+        className="navbar absolute top-0 flex items-center justify-beetween bg-black w-full mx-auto"
+      >
+        <div className="navbar-start mx-5 md:mx-20 my-10">
+          <Link to="/">
+            <img
+              src="/logo.png"
+              alt="M.T. Tasin"
+              className="w-[40px] h-[40px] md:w-[40px] md:h-[40px]"
+            />
+          </Link>
+        </div>
+        <div className="navbar-end mx-5 md:mx-20 my-10">
+          <TbMenuDeep
+            className="text-white text-3xl"
+            onClick={() => setIsOpen(!isOpen)}
+          />
         </div>
       </Navbar>
-      <Drawer open={isOpen} onClose={handleClose} className="w-full duration-700 league-spartan ">
+      <Drawer
+        open={isOpen}
+        onClose={handleClose}
+        className="w-full duration-700 league-spartan "
+      >
         {/* <h2 className="text-3xl text-black">Menu</h2> */}
         <div className="h-full w-full grid grid-cols-1 md:grid-cols-2">
-            <div className="grid grid-cols-6">
-              <div className="col-span-1 flex flex-col justify-start items-end mt-24 md:mt-28"><h2 className="-rotate-90 text-3xl">Menu</h2></div>
+          <div className="grid grid-cols-6">
+            <div className="col-span-1 flex flex-col justify-start items-end mt-24 md:mt-28">
+              <h2 className="-rotate-90 text-3xl">Menu</h2>
+            </div>
             <div className="col-span-5 flex flex-col justify-center items-start md:text-7xl text-6xl font-bold md:m-20">
-                <NavLink onClick={handleClose} className="my-5" to="/">Home</NavLink>
-                <NavLink onClick={handleClose} className="my-5" to="/projects">Projects</NavLink>
-                <NavLink onClick={handleClose} className="my-5" to="/about">About</NavLink>
-                <NavLink onClick={handleClose} className="my-5" to="/contact">Contact</NavLink>
+              <NavLink onClick={handleClose} className="my-5" to="/">
+                Home
+              </NavLink>
+              <NavLink onClick={handleClose} className="my-5" to="/projects">
+                Projects
+              </NavLink>
+              <NavLink onClick={handleClose} className="my-5" to="/about">
+                About
+              </NavLink>
+              <NavLink onClick={handleClose} className="my-5" to="/contact">
+                Contact
+              </NavLink>
             </div>
+          </div>
+          <div className="flex flex-col items-center justify-start md:justify-end md:my-32  text-xl md:text-3xl text-center">
+            <div className="my-3">m.t.tasin20@gmail.com</div>
+            <div className="my-3">+880 1974 283081</div>
+            <div className="my-3 flex flex-row justify-center md:justify-start gap-5">
+              <a href="https://x.com/MTTasin1" target="_blank">
+                <FaXTwitter />
+              </a>{" "}
+              <a href="https://github.com/MTTasin" target="_blank">
+                <FiGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/mttasin/">
+                <FaLinkedin />
+              </a>
             </div>
-            <div className="flex flex-col items-center justify-start md:justify-end md:my-32  text-xl md:text-3xl text-center">
-                <div className="my-3">m.t.tasin20@gmail.com</div>
-                <div className="my-3">+880 1974 283081</div>
-                <div className="my-3 flex flex-row justify-center md:justify-start gap-5">
-                  <a href="https://x.com/MTTasin1" target="_blank"><FaXTwitter /></a> <a href="https://github.com/MTTasin" target="_blank"><FiGithub /></a>
-                </div>
-            </div>
+          </div>
         </div>
-        <div className="absolute top-5 right-5"><FaX onClick={handleClose} className="text-3xl md:m-14" /></div>
+        <div className="absolute top-5 right-5">
+          <FaX onClick={handleClose} className="text-3xl md:m-14" />
+        </div>
       </Drawer>
     </>
-  )
+  );
 }
